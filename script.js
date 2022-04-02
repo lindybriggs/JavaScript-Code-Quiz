f79ff879c16bdf31778cd57072ae70bcc2670ae4
Lindy Briggs <briggs.lindy@yahoo.com>
2022-04-02 17:23:21 -0500

    built first question, displays upon click

--- script.js
+++ script.js
@@ -16,22 +16,56 @@ function timerId() {
 
 // GIVEN I am taking a code quiz...
 
+let welcomeMessage = document.querySelector(".welcome");
+
 // STEP 1:
     // WHEN I click the start button
     // THEN a timer starts and I am presented with a question
 let startButton = document.querySelector(".beginButton");
 
-let startQuestions = function(){
+let newAttempt = function(){
     timerId();
     console.log("hello");
-}
-startButton.addEventListener("click", startQuestions);
+    welcomeMessage.style.display = "none";
 
+    startQuiz();
+}
+startButton.addEventListener("click", newAttempt);
+
+function startQuiz(){
+    askQuestions();
+}
 
 
 // STEP 2:
     // WHEN I answer a question
     // THEN I am presented with another question
+
+let question = document.querySelector("#question");
+let choiceA = document.querySelector(".a");
+let choiceB = document.querySelector(".b");
+let choiceC = document.querySelector(".c");
+let choiceD = document.querySelector(".d");
+let currentQuestion = 0
+
+
+let questions = [
+    {
+        question: "Which HTML tag do we use to link Javascript files?",
+        choices: ["a. <h1>", "b. <link>", "c. <script>", "d. <meta>"],
+        answer: "c. <script>",
+    }
+]
+
+function askQuestions(){
+    question.textContent = questions[currentQuestion].question;
+    choiceA.textContent = questions[currentQuestion].choices[0];
+    choiceB.textContent = questions[currentQuestion].choices[1];
+    choiceC.textContent = questions[currentQuestion].choices[2];
+    choiceD.textContent = questions[currentQuestion].choices[3];
+
+}
+
 
 // STEP 3:
     // WHEN I answer a question incorrectly
@@ -40,6 +74,7 @@ startButton.addEventListener("click", startQuestions);
 // STEP 4:
     // WHEN all questions are answered or the timer reaches 0
     // THEN the game is over
+    // (probably do an if statement, with timer equaling zero & questions being done-idk how yet-and statement brings to scoreboard.html)
 
 // STEP 5:
     // WHEN the game is over
